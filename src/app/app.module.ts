5a6bbae59cfc10836150ac33e1870ffca898a833
Emerson Jacobson <emwjacobson@gmail.com>
2020-01-11 16:55:42 -0800

    Removed reference to deleted component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ToggleComponent } from './components/toggle/toggle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ToggleComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
